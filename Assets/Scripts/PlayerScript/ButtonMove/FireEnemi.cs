652dc9d02ea636cfa77c6ee690c9fa6a0db66ede
KolyaKolya95 <kolya.kolya1495@gmail.com>
2017-12-02 00:27:48 +0200

    first commit

---
+++ Assets/Scripts/PlayerScript/ButtonMove/FireEnemi.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireEnemi : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D fire)
+	{
+		if (fire.gameObject.tag ==  "Fire") 
+		{
+			print ("OK");
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerScript/ButtonMove/MovePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+
+	public float speed = 20f;
+	private Rigidbody2D rb;
+
+
+	void Start () {
+		rb = GetComponent <Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float movexY = Input.GetAxis ("Vertical");
+		rb.MovePosition (rb.position + Vector2.up * movexY * Time.deltaTime);
+	}
+}
